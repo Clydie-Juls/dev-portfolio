a22253a00927f22a711f1e9ebea6951bc8692035
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-11-20 17:58:54 +0800

    feat: add actual descriptions to projects

--- components/custom/project-bento-grid.tsx
+++ components/custom/project-bento-grid.tsx
@@ -19,40 +19,41 @@ export function ProjectBentoGrid() {
 const Skeleton = () => (
   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
 );
+
 const items = [
   {
     title: "GDSC Events",
-    description: "I did Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed risus mollis lectus eleifend aliquet eget nec sem. Cras pharetra varius tortor ut scelerisque.",
+    description: "A web-based full-stack even transaction manager and summarizer for the DLSU google Developers’ student using Next.js and Supabase. Being made in partial fulfillment of CSSWENG",
     header: <Skeleton />,
     className: "md:col-span-2",
   },
   {
     title: "Action Recognition Sign Language", 
-    description: "I did Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed risus mollis lectus eleifend aliquet eget nec sem. Cras pharetra varius tortor ut scelerisque.",
+    description: "A camera-based action recognition sign language application written in Python. this application uses mediaPipe for hand tracking, OpenCv for image processing and recording, and LSTM model using Tensorflow for action recognition.",
     header: <Skeleton />,
     className: "md:col-span-1",
   },
   {
     title: "Family Tree Chatbot",
-    description: "I did Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed risus mollis lectus eleifend aliquet eget nec sem. Cras pharetra varius tortor ut scelerisque.",
+    description: "nfers family tree relationships using a logic-based model and inference rules in Prolog. Made in partial fulfillment of CSINTSY.",
     header: <Skeleton />,
     className: "md:col-span-1",
   },
   {
     title: "Boogie Post",
-    description: "I did Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed risus mollis lectus eleifend aliquet eget nec sem. Cras pharetra varius tortor ut scelerisque.",
+    description: "A simple blog application with blog posting with image upload and viewing  made using Javascript, Express.js and EJS",
     header: <Skeleton />,
     className: "md:col-span-2",
   },
   {
     title: "TTT",
-    description: "I did Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed risus mollis lectus eleifend aliquet eget nec sem. Cras pharetra varius tortor ut scelerisque.",
+    description: "A forum applciation with jwt authentication and authorization using React.js and MongoDb. Made in partial fullfillment of CCAPDEV",
     header: <Skeleton />,
     className: "md:col-span-2",
   },
   {
     title: "Sokobot",
-    description: "I did Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed risus mollis lectus eleifend aliquet eget nec sem. Cras pharetra varius tortor ut scelerisque.",
+    description: "A midtier sokobot solver in Java using GBFS, Zorbist Hashing, and lower-bound heuristic deadlock detection. Made in partial fulfillment of CSINTSY.",
     header: <Skeleton />,
     className: "md:col-span-1",
   },
