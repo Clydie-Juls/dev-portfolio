16b1a04c4799af3d738c142b153169cb075bc272
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-11-21 01:22:10 +0800

    fix: fix social icon link in footer

--- components/custom/footer.tsx
+++ components/custom/footer.tsx
@@ -5,14 +5,14 @@ export default function Footer() {
   return (
     <div className="container mx-auto flex flex-col items-center justify-center p-10">
         <div className="w-full flex items-center justify-center gap-8">
-            <Link target="_blank" href="https://www.facebook.com/clyde.marindo" passHref>
-                <SocialIcon url="www.facebook.com" bgColor="transparent" fgColor="white"/>
+            <Link target="_blank" href="https://www.facebook.com/clyde.marindo" passHref legacyBehavior>
+                <SocialIcon url="www.facebook.com" bgColor="transparent" fgColor="white" target="_blank"/>
             </Link>
-            <Link target="_blank" href="https://www.linkedin.com/in/clyde-julian-marindo-3a7888275/" passHref>
-                <SocialIcon url="www.linkedin.com" bgColor="transparent" fgColor="white"/>
+            <Link target="_blank" href="https://www.linkedin.com/in/clyde-julian-marindo-3a7888275/" passHref legacyBehavior>
+                <SocialIcon url="www.linkedin.com" bgColor="transparent" fgColor="white" target="_blank"/>
             </Link>
-            <Link target="_blank" href="https://github.com/Clydie-Juls" passHref>
-                <SocialIcon url="https://github.com" bgColor="transparent" fgColor="white"/>
+            <Link target="_blank" href="https://github.com/Clydie-Juls" passHref legacyBehavior>
+                <SocialIcon url="https://github.com" bgColor="transparent" fgColor="white" target="_blank"/>
             </Link>
         </div>
         <div className="w-full flex items-center justify-center gap-4 my-4">
