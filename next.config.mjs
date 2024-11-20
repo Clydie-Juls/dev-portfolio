1e18f50438709c42055619f8066780ea668ca5e4
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-11-21 01:50:40 +0800

    fix: ignore eslint only in production

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['github.com', 'pbs.twimg.com', 'images.unsplash.com'],
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
